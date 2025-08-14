6f9d689054cef6d1e979a34a8c8315157708691f
mohamed <mohamed.icho.99@gmail.com>
2025-08-14 07:45:01 +0100

    edited main

--- dynamic_strings/main.c
+++ dynamic_strings/main.c
@@ -38,7 +38,7 @@ t_string *ft_newstr(char *s)
 
 int is_delimiter(char c)
 {
-	if (c == '"' || c == ' ' || c == '|' || c == '>' || c == '<')
+	if (c == '"' || c == ' ' || c == '|' || c == '>' || c == '<' || c == '\'')
 		return (1);
 	return (0);
 }
@@ -66,36 +66,28 @@ char *tokanize_word(t_string *str)
 	return (s);
 }
 
-// this function should start from str->q_pos 
-// and keep moving inside str->str until w_q_len
-char *tokanize_inside_quote(t_string *str)
+char *create_quote_word(t_string *str)
 {
-	// maybe this function is the one causing the mess
-	// the indexing here is the problem maybe
-	char *s = malloc(sizeof(char) * (str->w_q_len + 1));
-	if (!s)
-		return NULL;
-	ft_memcpy(s, str->str + str->q_pos + 1, str->w_q_len);
-	s[str->w_q_len] = '\0';
-	// or maybe add that 2 only under a condition
-	str->peek += str->w_q_len; // + 2
-	return (s);
-}
-
-void handle_quote(t_string *str)
-{
+	int len;
 	char *s;
 
 	s = NULL;
-	str->w_q_len = str->peek;
-	str->q_pos = str->peek;
-	while (str->w_q_len + 1 < str->len && str->str[str->w_q_len + 1] != str->quote)
-		str->w_q_len++;
-
-	str->w_q_len -= str->q_pos;
-	//printf("l: %d\n", str->w_q_len);
-	s = tokanize_inside_quote(str);
-	printf("s: %s\n", s);
+	str->peek++;
+	str->start = str->peek;
+	str->end = str->peek;
+	while (str->peek < str->len && str->str[str->peek] != str->quote)
+	{
+		str->end++;
+		str->peek++;
+	}
+	len = str->end - str->start;
+	s = malloc(sizeof(char) * (len + 1));
+	if (!s)
+		return (NULL);
+	ft_memcpy(s, str->str + str->start, len);
+	s[len] = '\0';
+	str->peek++;
+	return (s);
 }
 
 void collect_delimiter(t_string *str)
@@ -120,10 +112,25 @@ void collect_delimiter(t_string *str)
 	s[len] = '\0';
 	printf("d: %s\n", s);
 }
+
+
+void handle_delimiter(t_string *str)
+{
+	char *s = NULL;
+	if (str->str[str->peek] == '"' || str->str[str->peek] == '\'')
+	{
+		str->quote = str->str[str->peek];
+		s = create_quote_word(str);
+	}
+	else
+		collect_delimiter(str);
+	if (s)
+		printf("qw: %s\n", s);
+}
+
 void pc(t_string *str)
 {
-	char *s;
-	s = NULL;
+	char *s = NULL;
 	while (str->str[str->peek] == ' ')
 		str->peek++;
 	while (str->peek < str->len)
@@ -134,28 +141,7 @@ void pc(t_string *str)
 			continue ;
 		}
 		if (is_delimiter(str->str[str->peek]) && str->str[str->peek] != ' ')
-		{
-			if (str->str[str->peek] == '"')
-			{
-				if (str->quote == '\0')
-				{
-					str->quote = '"';
-					handle_quote(str);
-				}
-				else
-				{
-					str->quote = '\0';
-					//str->peek++;
-				}
-			}
-			if (str->str[str->peek] == ' ')
-			{
-				str->peek++;
-				continue ;
-			}
-			collect_delimiter(str);
-			continue ;
-		}
+			handle_delimiter(str);
 		else
 		{
 			s = tokanize_word(str);
@@ -175,10 +161,15 @@ void set_def(t_string *str)
 	str->w_q_len = 0;
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
-	//char *input = "   ls -l | cat file.txt >> here.txt |||||    echo \"hello world\"   \"karim is here\"<<<<<<<";
-	char *input = "   ls -l | cat file.txt >> here.txt |||||    echo \"hello world\"   <<<<<<<";
+	char *input = "   ls -l | cat file.txt >><here.txt |||||    echo \'hello world\'   \"karim is here\"<<<<<<<";
+	input = argv[1];
+	//char *input = "   ls -l | cat file.txt >><here.txt |||||    echo \"hello world\"   \"karim is here\"<<<<<<<";
+	//input = " echo \"hello world\" c <<<<<>";
+	//input = "   echo \"hello world\"   \"karim is here\"<<<<<<< ls -l || cat file.txt >><here.txt |||||    ";
+	printf("%s\n", input);
+	//char *input = "   ls -l | cat file.txt >> here.txt |||||    echo \"hello world\"   <<<<<<<";
 	//char *input = "   ls -l | cat file.txt >> here.txt |||||    \"echo\"\"hello world\"\"\"\"\"\"\"   <<<<<<<";
 
 	t_string *str = ft_newstr(input);
